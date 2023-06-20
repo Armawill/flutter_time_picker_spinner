60aa6c737104b343e6ef9b66edd219ef1ccf1626
Armawill <armawill@gmail.com>
2023-06-20 16:39:33 +0300

    Added foreground grey with opacity when isEnable is false

--- lib/flutter_time_picker_spinner.dart
+++ lib/flutter_time_picker_spinner.dart
@@ -323,13 +323,15 @@ class _TimePickerSpinnerState extends State<TimePickerSpinner> {
       ));
     }
 
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: Container(
-        foregroundDecoration:
-            BoxDecoration(color: Colors.grey.withOpacity(0.5)),
-        child: contents,
+    return Container(
+      foregroundDecoration: BoxDecoration(
+          color: widget.isEnable
+              ? Colors.white.withOpacity(0.0)
+              : Colors.white.withOpacity(0.4)),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: contents,
       ),
     );
   }
